60e734b118344455a5e7d3b561de65822f75eb61
daminz1997 <darrendamin@gmail.com>
2020-02-27 10:13:41 +0800

    test

--- initial/montageDigits.m
+++
@@ -1,9 +0,0 @@
-function avgImage = montageDigits(x)
-numImages = size(x, 4);
-nx = 20;
-ny = ceil(numImages/nx);
-montage(x, 'Size', [ny nx]);
-
-if nargout > 0, %compute avgImg
-    avgImage = squeeze(mean(x, 4));
-end
